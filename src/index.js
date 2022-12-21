9d3bf2c0f32e5df21ff625f7f14b05dd8265bf82
Garam-Leee <grlee0806@gmail.com>
2022-12-21 18:58:13 +0900

    feat / 기본 레이아웃 구성 및 Router 구현

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NotFoundPage from "./pages/404";
+import "./css/global.css";
+import Home from "./pages/home";
+import News from "./pages/news";
+import Community from "./pages/community";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/community" element={<News />} />
+        <Route path="/news" element={<Community />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
